96f8c48572adfbb46482ec2fc1b33e7d0443c612
Joeri Hendrickx <jh.cube@gmail.com>
2017-09-17 22:29:33 +0200

    Add service for filters

---
+++ moola-webapp/src/main/webapp/filter.service.js
@@ -0,0 +1,51 @@
+angular.module('moola').service('FilterService', ['$http', 'config', function($http, config){
+
+    var self = this;
+
+    self.SUBJECT_PEER = 'peer';
+    self.SUBJECT_CATEGORY = 'category';
+
+    self.saveNewFilter = function(expression, subjectType, subject, applyMode){
+
+        var data = {
+            expression: expression,
+            peerId: subject.id,
+            apply: applyMode
+        }
+
+        if (subjectType === self.SUBJECT_PEER)
+            data.peerId = subject.id
+        else if (subjectType === self.SUBJECT_CATEGORY)
+            data.categoryId = subject.id
+        else throw `Unsupported type: ${subjectType}`;
+
+        return $http.post(url('/filters/new'), data)
+            .then(unwrapResponse)
+            .catch(unwrapError);
+    }
+
+    var unwrapResponse = function (response) {
+        return response.data;
+    };
+
+    var unwrapError = function (response) {
+        throw response.data;
+    };
+
+    var url = function (relative, queryParameters) {
+        var r = config.apiUrl + relative;
+        if (queryParameters && Object.keys(queryParameters).length > 0) {
+            r += '?' + Object
+                .keys(queryParameters)
+                .filter(function (key) {
+                    return !!queryParameters[key];
+                })
+                .map(function (key) {
+                    return encodeURIComponent(key) + '=' + encodeURIComponent(queryParameters[key]);
+                })
+                .join('&');
+        }
+        return r;
+    };
+
+}]);
